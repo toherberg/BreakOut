76f6f0041d5e15909cab8694e676e53ff2a9f4b0
toherberg <tth4ik@gmail.com>
2016-04-15 13:38:41 +0300

    added setup without bricks - ball, moving paddle

--- src/com/gmail/tth4ik/BreakOut.java
+++ src/com/gmail/tth4ik/BreakOut.java
@@ -1,13 +1,20 @@
 package com.gmail.tth4ik;
 
+import java.awt.Color;
 import java.awt.Event;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
+import acm.graphics.GPoint;
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
 public class BreakOut extends GraphicsProgram {
+
+	// Кольори рядків цеглинок (зверху вниз) - RED(2 верхні рядки) ORANGE,
+	// YELLOW, GREEN, CYAN (2 нижні рядки.
 
 	/** Width and height of application window in pixels */
 	public static final int APPLICATION_WIDTH = 400;
@@ -49,47 +56,91 @@ public class BreakOut extends GraphicsProgram {
 	private static final int NTURNS = 3;
 
 	private static int bricksQuantity = 100;
-	
+
 	private static int lifes = 3;
 
 	public void run() {
 		setup();
-		while (!gameOver()) {
-			if (ball==null){
+		// while (!gameOver()) {
+		if (ball == null) {
 			createBall();
 			throwBall();
-			}
-			checkForCollisions();
-			moveBall();
+			// }
+			// checkForCollisions();
+			// moveBall();
 		}
+
 	}
-	
+
+	/**
+	 * Метод налаштовує гру - створює вікно гри, цеглинки, які слід розбивати,
+	 * ракетку і додає слухачів дій мишки
+	 */
 	private void setup() {
+		this.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 		createField();
 		createRaquet();
 		addMouseListeners();
 
 	}
-	
-	public void mouseDragged(MouseEvent e) { 
-		System.out.println("hey hey, you clicked mouse");
-	}
 
+	/** метод створює м'ячик за заданими параметрами */
 	private void createBall() {
-		// створює м'ячик, ставить його у певну позицію
+		ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+		ball.setFilled(true);
+		ball.setFillColor(Color.RED);
+		ball.setColor(Color.RED);
+		add(ball, APPLICATION_WIDTH / 2 - BALL_RADIUS, APPLICATION_HEIGHT / 2 - BALL_RADIUS);
 
 	}
 
+	/** метод створює ракетку за заданими параметрами */
 	private void createRaquet() {
-		// створює ракетку, ставить її у певну позицію
+		paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setFillColor(Color.BLACK);
+		add(paddle, APPLICATION_WIDTH / 2 - PADDLE_WIDTH / 2, APPLICATION_HEIGHT - PADDLE_HEIGHT - PADDLE_Y_OFFSET);
+	}
 
+	/**
+	 * метод фіксує об'єкт на який клікнув користувач (цей метод потрібен для
+	 * руху ракетки). Якщо це не пустий об'єкт - метод фіксує точку на якій він
+	 * знаходиться і фіксує її як попередню координату
+	 */
+	public void mousePressed(MouseEvent e) {
+		obj = getElementAt(e.getX(), e.getY());
+		if (obj != null) {
+			last = new GPoint(e.getPoint());
+		}
+	}
+
+	/**
+	 * Метод рухає ракетки під час "перетягування мишки". Для цього потрібно
+	 * просто зажати кнопку миші, навівши її на ракетку. Подібно м'ячику -
+	 * ракетка відбивається від стін. При досяганні правої і лівої рамок вікна -
+	 * її злегка відбиває
+	 */
+	public void mouseDragged(MouseEvent e) {
+		if ((getElementAt(e.getX(), e.getY()) == null) || (getElementAt(e.getX(), e.getY()) != paddle)) {
+			return;
+		} else {
+			if (obj.getX() < 0) {
+				obj.move(5, 0);
+				return;
+			}
+			if (obj.getX() >= APPLICATION_WIDTH - PADDLE_WIDTH) {
+				obj.move(-5, 0);
+				return;
+			}
+			obj.move(e.getX() - last.getX(), 0);
+			last = new GPoint(e.getPoint());
+		}
 	}
 
 	private void createField() {
 		// створює поле - набір цеглинок
 
 	}
-	
 
 	private void moveBall() {
 		// метод, який рухає м'ячик у певному напрямі
@@ -105,7 +156,7 @@ public class BreakOut extends GraphicsProgram {
 	private boolean gameOver() {
 		if (bricksQuantity == 0)
 			return true;
-		if (lifes==0)
+		if (lifes == 0)
 			return true;
 		return false;
 	}
@@ -138,8 +189,9 @@ public class BreakOut extends GraphicsProgram {
 
 	}
 
-
-	
-	GOval ball;
+	private GOval ball;
+	private GRect paddle;
+	private GObject obj;
+	private GPoint last;
 
 }
